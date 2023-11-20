e6367dfc1be671cb253c80ee823a1a394336a4a8
Bytecode666 <151552809+Bytecode666@users.noreply.github.com>
2023-11-20 18:32:33 +0000

    Create Deobfuscate.ps1

---
+++ Deobfuscate.ps1
@@ -0,0 +1,26 @@
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$openFileDialogInput = New-Object System.Windows.Forms.OpenFileDialog
+$openFileDialogInput.Title = "Select Input File"
+$openFileDialogInput.Filter = "All Files (*.*)|*.*"
+
+$resultInput = $openFileDialogInput.ShowDialog()
+
+if ($resultInput -eq [System.Windows.Forms.DialogResult]::OK) {
+    $inputFilePath = $openFileDialogInput.FileName
+    $saveFileDialogOutput = New-Object System.Windows.Forms.SaveFileDialog
+    $saveFileDialogOutput.Title = "Save Output File"
+    $saveFileDialogOutput.Filter = "All Files (*.*)|*.*"
+    $resultOutput = $saveFileDialogOutput.ShowDialog()
+    if ($resultOutput -eq [System.Windows.Forms.DialogResult]::OK) {
+        $outputFilePath = $saveFileDialogOutput.FileName
+        $bytes = [System.IO.File]::ReadAllBytes($inputFilePath)
+        $bytes = $bytes[8..($bytes.Length - 1)] # slices like i dont know how to explain first 8 bites gets ykyk..
+        [System.IO.File]::WriteAllBytes($outputFilePath, $bytes)
+        Start-Process "notepad.exe" -ArgumentList $outputFilePath -Wait -WindowStyle Maximized
+        return
+    }
+} else {
+    [System.Windows.Forms.MessageBox]::Show("No input file selected idi...", "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+}
