541ab0652086246a6fbc1b174bf318744b2e0007
EvilBytecode <151552809+EvilBytecode@users.noreply.github.com>
2023-11-21 05:59:53 +0000

    Update Deobfuscate.ps1

--- Deobfuscate.ps1
+++ Deobfuscate.ps1
@@ -17,6 +17,11 @@ if ($resultInput -eq [System.Windows.Forms.DialogResult]::OK) {
         $outputFilePath = $saveFileDialogOutput.FileName
         $bytes = [System.IO.File]::ReadAllBytes($inputFilePath)
         $bytes = $bytes[8..($bytes.Length - 1)] # slices like i dont know how to explain first 8 bites gets ykyk..
+        $deobfuscatedCode = [System.Text.Encoding]::UTF8.GetString($bytes)
+        $atIndex = $deobfuscatedCode.IndexOf('@')
+        $modifiedCode = $deobfuscatedCode.Insert($atIndex, 'echo ')
+        $bytes = [System.Text.Encoding]::UTF8.GetBytes($modifiedCode)
+        
         [System.IO.File]::WriteAllBytes($outputFilePath, $bytes)
         Start-Process "notepad.exe" -ArgumentList $outputFilePath -Wait -WindowStyle Maximized
         return
